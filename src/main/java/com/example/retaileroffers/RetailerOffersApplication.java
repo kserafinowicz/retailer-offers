6e6fdbcebc5baf55e3c1e608ee3d79a0032d1c6c
serafinowicz.k@gmail.com <serafinowicz.k@gmail.com>
2023-07-04 22:13:20 +0200

    Added Spring initial project structure. Added README.md with description.

---
+++ src/main/java/com/example/retaileroffers/RetailerOffersApplication.java
@@ -0,0 +1,13 @@
+package com.example.retaileroffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailerOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailerOffersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/retaileroffers/RetailerOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retaileroffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerOffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
